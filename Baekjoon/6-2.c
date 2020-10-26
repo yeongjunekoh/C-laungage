0e53b90eaf92a545f8b1b0f45bed30de42f2b3d4
yeongjunekoh <rhdudwns0@naver.com>
2020-10-26 13:09:15 +0900

    6-2 성공

--- Baekjoon/6-2.c
+++ Baekjoon/6-2.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 
 int d(int n){
-    int count = 0;
-    int k = 0;
-    for(int i = 0; ; i++){
-        k = n / 10;
-        if(k < 1) break;
-        count++;
+    int count = n;
+    int k;
+    while(n >= 1){
+        k = n % 10;
+        count = count + k;
+        n = n / 10;
     }
-    k = n;
-    for(int i = 0; ; i++){
-        if(count == 0) break;
-        k = k/(10^count);
-        n = n + k;
-        count--;
-    }
-    return n;
+    return count;
 }
 
 int main(void){
@@ -25,12 +18,14 @@ int main(void){
     }
     int n = 1;
     for(int i = 1; i < 10000; i++){
-        for(int j = 1; ; j++){
+        n = i;
+        while(n < 10000){
+            int k = n;
             n = d(n);
-            if(a[n] == n) a[n] = 0;
             if(n >= 10000) break;
+            if(k == n) break;
+            if(a[n] == n) a[n] = 0;
         }
-        n++;
     }
     for(int i = 0; i < 10000; i++){
         if(a[i] != 0) printf("%d\n", a[i]);
