d7dad090df8cddb2dcca87a39e1436630bd142ac
yeongjunekoh <rhdudwns0@naver.com>
2020-11-09 15:40:25 +0900

    7-5

--- Baekjoon/7-5.c
+++ Baekjoon/7-5.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-int Max(int res[]){
+int findout_max(int res[]){
     int Max = 0;
     for(int i = 0; i < 26; i++){
         if(res[i] >= Max){
@@ -11,26 +11,31 @@ int Max(int res[]){
     return Max;
 }
 
-char result(int res[],int Max){
+void findout_double(int res[],int Max){
     int count = 0;
+    int n = 0;
     for(int i = 0; i < 26; i++){
         if(res[i] == Max){
             count++;
+            n = i + 65;
         }
     }
     if(count > 1){
-        return '?';
+        printf("?");
     }else{
-        return Max;
+        printf("%c", n);
     }
 }
 
 int main(void){
-    char s1[] = {0,};
+    char s1[1000001] = {0,};
     scanf("%s", s1);
-    char A[52];
-    for(int i = 0; i < 52; i++){
+    int A[52];
+    for(int i = 0; i < 26; i++){
         A[i] = 'A' + i;
+    }
+    for(int i = 0; i < 26; i++){
+        A[i + 26] = 'a' + i;
     }
     int res[52] = {0,};
     for(int i = 0; i < 52; i++){
@@ -43,10 +48,8 @@ int main(void){
     for(int i = 0; i < 26; i++){
         res[i] = res[i] + res[i + 26];
     }
-    int MAX = 0;
-    MAX = Max(res[26]);
-    char res = 0;
-    res = result(res[26], Max);
 
-    printf("%c", res);
+    int MAX = 0;
+    MAX = findout_max(res);
+    findout_double(res, MAX);
 }
